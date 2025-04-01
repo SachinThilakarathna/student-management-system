d9cb29fdb2805ea82c592159f86ceca479295c07
Sachin Thilakarathna <thilakarathnasachin2003@gmail.com>
2025-04-01 12:56:43 +0530

    Update about file

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -11,7 +11,7 @@ function About() {
         style={{ backgroundImage: "url('section-background.jpg')" }}
       >
         <Body bodyname="About">
-          <div className="max-w-4xl mx-auto pt-10">
+          <div className="max-w-4xl mx-auto pt-10 pb-10">
             <section className="mb-22  p-6 rounded-lg shadow-lg mx-5  ">
               <h2 className="text-2xl md:text-2xl lg:text-3xl font-semibold text-[#fefcff] mb-6">About Our Student Management System</h2>
               <p className="text-base md:text-lg lg:text-1xl text-gray-300 leading-relaxed">
@@ -22,7 +22,7 @@ function About() {
                 ensuring real-time data management."
               </p>
             </section>
-            <section className="bg-[#9e9e9e5b] p-6 rounded-lg shadow-lg ">
+            <section className="bg-[#9e9e9e5b] p-6 rounded-lg shadow-lg">
               <h2 className="text-2xl md:text-2xl lg:text-3xl font-semibold text-[#ffffff] mb-6">Our Vision</h2>
               <p className="text-base md:text-lg lg:text-1xl  text-[#ffffff] leading-relaxed">
                 "To provide an efficient, user-friendly, and cloud-based solution
